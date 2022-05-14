679dd9509a25ccb3e03b4240d85f3696bc2362a0
tonyqiu123 <tonyqiu12345@gmail.com>
2022-05-14 10:56:31 -0400

    Added blog and contact page

--- projects.js
+++
@@ -1,7 +0,0 @@
-
-const projects = document.querySelector('.projects')
-    if (IsInView(projects)){
-        projects.style.opacity = '1'
-        projects.style.transform = 'unset'
-        projects.style.filter = 'unset'
-}
